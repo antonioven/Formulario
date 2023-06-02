4fa86ad0a3f255636e707c1edc953c5597727caa
antonio_martinez <antoniomtzventura24@gmail.com>
2023-06-01 17:37:01 -0700

    Tercer Commit-Javascript y otros ajustes de HTMLyCSS

---
+++ index.js
@@ -0,0 +1,29 @@
+function enviar(){
+    let Unombre = document.getElementById('nombre').value;
+    let Uapellido = document.getElementById('apellido').value;
+    let Uemail = document.getElementById('email').value;
+    let Ugender = document.getElementsByName("genero");
+
+    if(Unombre === ''){
+        alert("El campo de nombre no puede estar vacio favor de llenarlo");
+    }
+    if(Uapellido === ''){
+        alert("El campo de apellido no puede estar vacio favor de llenarlo");
+    }
+    if(Uemail === ''){
+        alert("El campo de email no puede estar vacio favor de llenarlo");
+    } 
+    if(!document.getElementById("hombre").checked && !document.getElementById("mujer").checked && !document.getElementById("nbin").checked){
+        alert("Selecciona una opcion de genero por favor");
+    }
+    if(!document.getElementById("tyc").checked){
+        alert("Favor de aceptar los terminos y condiciones para proseguir");
+    }
+    const object = {
+        nombre: Unombre,
+        apellido: Uapellido,
+        email: Uemail,
+    }
+    console.log(object);
+    
+}
